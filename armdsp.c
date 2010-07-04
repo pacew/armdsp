b642887b8ee5c6aab0cedb3e5a5e6f5e3c482fe2
Pace Willisson <pace@alum.mit.edu>
2010-07-03 23:24:42 -0400

    add license

--- armdsp.c
+++ armdsp.c
@@ -1,7 +1,24 @@
 /*
- * by Pace Willisson <pace@alum.mit.edu>
- *
- * inspired by dsploader.c by Tobias Knutsson, 2009-6-24
+    armdsp - a simple framework for developing dsp programs for the TI OMAP
+    Copyright (C) 2010  Pace Willisson <pace@alum.mit.edu>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * this module was partly inspired by dsploader.c by Tobias Knutsson,
+ * 2009-6-24
  */
 
 #include <linux/module.h>
